4d1e432383d424110cabb45d0e312de285c168d3
cgruver <cgruver@redhat.com>
2025-12-03 13:35:35 +0000

    add comments

--- workspace-image/README.md
+++ workspace-image/README.md
@@ -1,2 +1,5 @@
 # Nested Container Support
 
+This folder contains the manifests to create the container image that is used for this workspace - `quay.io/cgruver0/che/ocp-4-20-nested-containers:latest`
+
+The Containerfile and entrypoint have comments that explain the internals.
--- workspace-image/entrypoint.sh
+++ workspace-image/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# If the user's home dir does not exist, then create it.  this allows for configurable locations for user home.
 if [ ! -d "${HOME}" ]
 then
   mkdir -p "${HOME}"
 fi
 
+# Configure the container runtime to use an ephemeral graphroot, and to use fuse-overlay instead of vfs.
 if [ ! -d "${HOME}/.config/containers" ]
 then
   mkdir -p ${HOME}/.config/containers
